fdcf5659a15a1a18fd3469e6654033fda55ab401
Evan Swanson <evan.sundelius@gmail.com>
2022-08-06 16:50:51 -0500

    Finalize changes

--- oop.js
+++ oop.js
@@ -2,16 +2,46 @@
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "silver";
+  }
 
+  say(newPhrase) {
+    this.string = "*~* " + newPhrase + " *~*";
+  }
+};
 
+var uni1 = new Unicorn("Charlie");
+uni1.say("Howdy-Ho");
+console.log(uni1);
 
 // Write a class called Vampire
 // it should have a dynamic name property
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
+class Vampire {
+  constructor(name) {
+    this.name = name;
+    this.pet = "bat";
+    this.thirst = true
+  }
 
+  changePet(newPet) {
+    this.pet = newPet;
+  }
 
+  notThirsty() {
+    this.thirst = false;
+  }
+};
+
+var vamp1 = new Vampire("Down-Bad Vlad");
+vamp1.changePet("William");
+vamp1.notThirsty();
+console.log(vamp1);
 
 //  Write a Dragon class
 // it should have a dynamic name property (string)
@@ -20,8 +50,32 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+class Dragon {
+  constructor(name) {
+    this.name = name;
+    this.isHungry = true;
+  }
 
+  changeRider(rider) {
+    this.rider = rider;
+  }
 
+  changeColor(color) {
+    this.color = color;
+  }
+
+  eat(meals) {
+    if (meals >= 4) {
+      this.isHungry = false;
+    }
+  }
+};
+
+var dragon1 = new Dragon("Jeffrey");
+dragon1.changeRider("Jeb");
+dragon1.changeColor("jade");
+dragon1.eat(5);
+console.log(dragon1);
 
 //  BIG CHALLNEGE: Write a Hobbit class
 //  it should have a dynamic name property (string)
@@ -31,3 +85,36 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.isAdult = false;
+    if (this.age >= 33) {
+      this.isAdult = true;
+    }
+    this.isOld = false;
+    if (this.age >= 101) {
+      this.isOld = true;
+    }
+    this.hasRing = false;
+    if (name === "Frodo") {
+      this.hasRing = true;
+    }
+  }
+
+  changeDisposition(disposition) {
+    this.disposition = disposition;
+  }
+
+  celebrateBirthday() {
+    this.age++;
+    }
+
+}
+var hob1 = new Hobbit("Frodo");
+hob1.changeDisposition("cheery");
+for (var i = 0; i < 38; i++){
+hob1.celebrateBirthday();
+}
+console.log(hob1);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,10 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+Honestly, it put me in the most intense feeling of "stuck" I've had this week. It was frustrating and I struggled.. but I loved it. Despite (or maybe because of ..) this being such a difficult lesson, I had fun doing it.
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+Well. In this particular case, some stuff occurred that made it nigh impossible to really have a steady workflow.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+I'll ensure that my weeks are devoted to the course before anything else, and I'll spend these next two weeks brushing up on everything from Mod 0 to prepare.
